e15d006c4a7ff0f2c51682f19920ad903aeadb21
VIPIN KUSHWAHA <102202599+kushwahavipin@users.noreply.github.com>
2023-08-28 10:07:21 +0530

    Initial commit

---
+++ app/src/main/java/com/UdaiCoders/OnlineQuiz/helper/VolleyCallback.java
@@ -0,0 +1,8 @@
+package com.UdaiCoders.OnlineQuiz.helper;
+
+public interface VolleyCallback {
+    void onSuccess(boolean result, String message);
+
+    //void onSuccessWithMsg(boolean result, String message);
+
+}
